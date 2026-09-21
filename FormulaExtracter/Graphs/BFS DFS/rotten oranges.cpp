48b08b4c230d126655fdd1a7df6264353e26a433
Pranay Harishchandra <pranayharishchandra@gmail.com>
2026-09-22 02:18:05 +0530

    c

--- FormulaExtracter/Graphs/BFS DFS/rotten oranges.cpp
+++ FormulaExtracter/Graphs/BFS DFS/rotten oranges.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 // Function to calculate minimum time required to rot all fresh oranges
 int orangesRotting(vector<vector<int>>& grid) {
-
-    
     
     // If the grid is empty, return 0 as no oranges exist
     if (grid.empty()) return 0;
